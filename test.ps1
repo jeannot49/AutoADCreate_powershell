251cc5fe2b87946260f736cc26347a1b5db739da
jeannot49 <janou49430gmail.com>
2019-01-30 00:05:40 +0100

    Test de la fonction CreateMultipleUser

--- test.ps1
+++ test.ps1
@@ -12,36 +12,49 @@ Dernière modif.  :          2019-01-28 à 17:06
 # Inclusion de la librairie de fonctions
 . .\Library_CreateAD.ps1
 
-function CreateSimpleUser{
-    Param (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Name,                                 # Attribut GivenName de l'utilisateur dans l'AD
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Surname,                              # Attribut Surname de l'utilisateur dans l'AD
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $SamAccName,                           # Attribut SamAccountName de l'utilisateur dans l'AD
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Description                           # Attribut Description de l'utilisateur dans l'AD
-    )
-    $arobase = "@"
-    $password = Read-Host -Prompt "Entrez votre mot de passe " -AsSecureString
-    $OUusers = "OU=Utilisateurs,OU=Agence,DC=guitton,DC=fr"         # Attribut Path de l'utilisateur dans l'AD
-    $dnsroot = (Get-ADDomain).dnsroot
-    $upn = "$samaccname$arobase$dnsroot"                # Attribut UserPrincipalName de l'utilisateur dans l'AD
-    $fullname = "$name $surname"                        # Attribut Name de l'utilisateur dans l'AD
+function CreateMultipleUser {
+    $tabusers = Import-csv -Path .\new_users.csv -delimiter ";" # Importation du tableau contenant les utilisateurs à ajouter, dans les bonnes OU
+    $oudomain = (Get-ADDomain).distinguishedname
+    $usersdn = (Get-ADOrganizationalUnit -Filter "name -like 'Utilisateurs'").distinguishedname
+    Write-Host ""
+    foreach ($item in $tabusers) {
+        $username = $item.givenname
+        $usersurname = $item.surname
+        $usercontainer = $item.container
+        $userdescription = $item.description
+        $usersamaccname = $item.samaccname
 
-    New-ADUser `
-        -DisplayName $fullname `
-        -GivenName $name `
-        -Name $fullname `
-        -Surname $surname `
-        -SamAccountName $samaccname `
-        -UserPrincipalName $upn `
-        -AccountPassword $password `        # /!\ TODO : Vérifier que le mot de passe n'est plus stocké dans une variable après la fin du script
-        -Description $description `
-        -Company (Get-ADDomain).NetBIOSName `
-        -Path $OUusers `
-        -Enabled 1 `
-        -Verbose `
+        # Vérification de l'existence de l'utilisateur puis ajout s'il n'est pas déjà présent
+        $is_existing = (Get-ADUser -Filter "samaccountname -like '$usersamaccname'" -SearchBase "$oudomain").name
+        if ($is_existing -eq $null) {
+            CreateSimpleUser -Name $username -Surname $usersurname -SamAccName $usersamaccname -Description $userdescription -Container $usercontainer
+        }
+        else {
+            Write-Host "L'utilisateur $is_existing existe deja dans l'annuaire"
+            Write-Host ""
+        }
+        #Write-Host ""
+        # /!\ TODO : Réaliser un tableau Excel facilitant la construction des tous les attributs sans erreur
+        # Concatener afin d'avoir un SamAccountName dans Excel : =MINUSCULE(CONCATENER(GAUCHE(B2;1);C2))
+    }
 }
-CreateSimpleUser -Name Jean -Surname Guittoin -SamAccName "jguittoin" -Description "Compte de test"
+
+CreateMultipleUser
+
+<#                                                  Remarque 
+                                Création automatique avec changement de mdp à la première ouverture de session :
+New-ADUser `
+    -DisplayName "Jean Guittoin" `
+    -Name "Jean Guittoin" `
+    -GivenName "Jean" `
+    -Surname "Guittoin" `
+    -SamAccountName "jguittoin" `
+    -UserPrincipalName "jguittoin@guitton.fr" `
+    -Description "Compte automatique de test" `
+    -Company "GUITTON" `
+    -Path "OU=Utilisateurs,OU=Agence,DC=GUITTON,DC=FR" `
+    -Enabled 1 `
+    -ChangePasswordAtLogon 1 `
+    -PasswordNotRequired 1 `
+    -Verbose
+#>
