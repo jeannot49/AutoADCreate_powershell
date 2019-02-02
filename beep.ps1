8ed14337a4a7ceff0d7c17d9a10ede95cb08fc14
jeannot49 <janou49430@gmail.com>
2019-02-02 21:13:12 +0100

    beepbeep

--- beep.ps1
+++ beep.ps1
@@ -44,3 +44,24 @@ Start-Sleep -m 75
 Start-Sleep -m 150
 [console]::beep(466,150)
 [console]::beep(523,150)
+
+Wait-Event -Timeout 2
+
+[console]::beep(440,500)
+[console]::beep(440,500)
+[console]::beep(440,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,1000)
+[console]::beep(659,500)
+[console]::beep(659,500)
+[console]::beep(659,500)
+[console]::beep(698,350)
+[console]::beep(523,150)
+[console]::beep(415,500)
+[console]::beep(349,350)
+[console]::beep(523,150)
+[console]::beep(440,1000)
